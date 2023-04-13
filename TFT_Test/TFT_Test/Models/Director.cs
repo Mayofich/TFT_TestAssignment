d2849dd894fef7571116b8a879acb9c43c88817d
MajaS <maja.skripelj@outlook.com>
2023-04-13 13:30:29 +0200

    Changed some types to nullable (non-nullable ones will need to be entered when creating an entity)

--- TFT_Test/TFT_Test/Models/Director.cs
+++ TFT_Test/TFT_Test/Models/Director.cs
@@ -5,8 +5,8 @@
 		public int DirectorId { get; set; }
 		public string DirectorName { get; set; }
 		public string DirectorSurname { get; set; }
-		public ICollection<Film> FilmsDone { get; set; }
-		public ICollection<Film> FilmsInProgress { get; set; }
+		public ICollection<Film>? FilmsDone { get; set; }
+		public ICollection<Film>? FilmsInProgress { get; set; }
 		public string DirectorEmail { get; set; }
 		public string DirectorPassword { get; set; }
 
--- TFT_Test/TFT_Test/Models/Film.cs
+++ TFT_Test/TFT_Test/Models/Film.cs
@@ -7,10 +7,10 @@
 		public string FilmDescription { get; set;}
 		public TimeSpan FilmLengt { get; set; }
 		public DateTime StartedFilming { get; set; }
-		public DateTime EndedFilming { get; set; }
+		public DateTime? EndedFilming { get; set; }
 		public ICollection<Genre> FilmGenre { get; set;}
-		public ICollection<Invitation> ActorInvitations { get; set; }
-		public ICollection<Actor> ConfirmedActors { get; set; }
+		public ICollection<Invitation>? ActorInvitations { get; set; }
+		public ICollection<Actor>? ConfirmedActors { get; set; }
 		public Director FilmDirector { get; set; }
 
 	}
