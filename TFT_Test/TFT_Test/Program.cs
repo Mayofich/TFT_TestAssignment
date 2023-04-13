40a33a20f02a4b67798a010e8efafa943eb0d3bb
MajaS <maja.skripelj@outlook.com>
2023-04-13 13:08:57 +0200

    Trying to add Configuration in program.cs, installed Npgsql...

--- TFT_Test/TFT_Test/Program.cs
+++ TFT_Test/TFT_Test/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TFT_Test.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +14,18 @@ if (!app.Environment.IsDevelopment())
 	app.UseExceptionHandler("/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
+}
+
+//Accessing the Database NOTE: the instrunction link is using public void getting error "the modifier 'public' is not valid for this item"
+//public//
+ void ConfigureServices(IServiceCollection services)
+{
+    services.AddDbContext<ActorListContext>(
+        opts =>
+        {
+            opts.UseNpgsql(@"Server=localhost;Port=5432;Database=TFT_TestBase;
+                User ID=postgres;Password=pMajaolinkas88");
+        });
 }
 
 app.UseHttpsRedirection();
