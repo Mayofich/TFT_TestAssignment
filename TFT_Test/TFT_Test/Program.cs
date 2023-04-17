e0cd653e626a759619c8e5e33603c1d6e2d54c10
MajaS <maja.skripelj@outlook.com>
2023-04-17 13:24:11 +0200

    Added a new branch to test stuff

--- TFT_Test/TFT_Test/Program.cs
+++ TFT_Test/TFT_Test/Program.cs
@@ -39,11 +39,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//app.MapRazorPages();
+app.MapRazorPages();
 
 app.Run();
 /*
