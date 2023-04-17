57056010a1b83d3a69148bf0b056a8680b60d5d8
MajaS <maja.skripelj@outlook.com>
2023-04-17 13:28:54 +0200

    Managed to get my CRUD changes to work for the director through new generated Razor page using DirectorCRUDController

---
+++ TFT_Test/TFT_Test/Pages/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TFT_Test.Data;
+using TFT_Test.Models;
+
+namespace TFT_Test.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly TFT_Test.Data.DirectorCRUDContext _context;
+
+        public DetailsModel(TFT_Test.Data.DirectorCRUDContext context)
+        {
+            _context = context;
+        }
+
+      public Director Director { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Directors == null)
+            {
+                return NotFound();
+            }
+
+            var director = await _context.Directors.FirstOrDefaultAsync(m => m.DirectorId == id);
+            if (director == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Director = director;
+            }
+            return Page();
+        }
+    }
+}
--- TFT_Test/TFT_Test/Pages/Index.cshtml.cs
+++ TFT_Test/TFT_Test/Pages/Index.cshtml.cs
@@ -1,18 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using TFT_Test.Controlers;
+using Microsoft.EntityFrameworkCore;
+using TFT_Test.Data;
 using TFT_Test.Models;
 
 namespace TFT_Test.Pages
 {
-	public class IndexModel : PageModel
-	{
-		private readonly ILogger<IndexModel> _logger;
+    public class IndexModel : PageModel
+    {
+        private readonly TFT_Test.Data.DirectorCRUDContext _context;
 
-		public IndexModel(ILogger<IndexModel> logger)
-		{
-			_logger = logger;
-		}
-	}
+        public IndexModel(TFT_Test.Data.DirectorCRUDContext context)
+        {
+            _context = context;
+        }
 
+        public IList<Director> Director { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Directors != null)
+            {
+                Director = await _context.Directors.ToListAsync();
+            }
+        }
+    }
 }
